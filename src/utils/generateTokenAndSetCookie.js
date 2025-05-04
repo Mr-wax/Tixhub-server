7b29daf36590d5f7fd1da1188f25db1609fd4a08
Mr-wax <Ponsuknanchangjohn@gmail.com>
2025-05-04 05:58:49 -1100

    cookies =2 days

--- src/utils/generateTokenAndSetCookie.js
+++ src/utils/generateTokenAndSetCookie.js
@@ -1,16 +1,16 @@
 import jwt from "jsonwebtoken";
 
-const generateTokenAndSetCookie = (userId,res) => {
-	const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-    expiresIn: "15d",
-  });
-
+const generateTokenAndSetCookie = (userId, res) => {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1d" });
+  
 	res.cookie("jwt", token, {
-		httpOnly: true,
-		maxAge: 15 * 24 * 60 * 60 * 1000,
+	  httpOnly: true,
+	  secure: process.env.NODE_ENV === "production",
+	  sameSite: "strict",
+	  maxAge: 24 * 60 * 60 * 1000, // 1 day
 	});
-
+  
 	return token;
-};
-
+  };
+  
 export default generateTokenAndSetCookie;
